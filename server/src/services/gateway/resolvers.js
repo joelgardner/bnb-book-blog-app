eb92e4bf4552b42e639aca5604eab6184e848f59
Joel Gardner <william.joel.gardner@gmail.com>
2017-06-30 14:32:38 +0200

    add seneca

--- server/src/services/gateway/resolvers.js
+++ server/src/services/gateway/resolvers.js
@@ -1,22 +1,34 @@
-import { select, insert } from '../storage'
+// @flow
+import seneca from 'seneca'
 
 export async function getUser({ id }, context) {
-  try {
-    const results = await select('User', { _id: id })
-    return Object.assign(results[0], { id : results[0]._id })
-  }
-  catch (e) {
-    console.log(e)
-    throw e
-  }
+  return Promise.resolve({ id: 1, name: 'Billy Bob' })
 }
 
 export async function createUser(user, context) {
-  try {
-    return await insert('User', user)
-  }
-  catch (e) {
-    console.log(e)
-    throw e
-  }
+  return Promise.resolve({ id: 2, name: 'Billy Jean' })
 }
+
+
+// import { select, insert } from '../storage'
+
+// export async function getUser({ id }, context) {
+//   try {
+//     const results = await select('User', { _id: id })
+//     return Object.assign(results[0], { id : results[0]._id })
+//   }
+//   catch (e) {
+//     console.log(e)
+//     throw e
+//   }
+// }
+//
+// export async function createUser(user, context) {
+//   try {
+//     return await insert('User', user)
+//   }
+//   catch (e) {
+//     console.log(e)
+//     throw e
+//   }
+// }
