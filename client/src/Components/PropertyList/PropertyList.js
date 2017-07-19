50bc9e527c2807b638930f61248cc51559dd9ff3
Joel Gardner <william.joel.gardner@gmail.com>
2017-07-19 13:38:08 +0200

    use a HOC to prevent unnecessary rerenders

--- client/src/Components/PropertyList/PropertyList.js
+++ client/src/Components/PropertyList/PropertyList.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import PropertyListItem from '../PropertyListItem/PropertyListItem'
 
-const PropertyList = ({ immutable, onFetchMore, onToggleSort }) => {
-  const properties = batchesToProperties(
-    immutable.get('batches'),
-    immutable.get('showing')
-  )
-  return (
-    <div className="properties-container">
-      <div className="columns is-multiline">
-        {properties.map(property =>
-          <PropertyListItem key={property.id} {...property} />
-        )}
-      </div>
-      <button onClick={() => onFetchMore()}>Show More</button>
-      <button onClick={() => onToggleSort()}>Toggle Sort</button>
+const PropertyList = ({ properties, onFetchMore, onToggleSort }) =>
+  <div className="properties-container">
+    <div className="columns is-multiline">
+      {properties.map(property =>
+        <PropertyListItem key={property.id} {...property} />
+      )}
     </div>
-  )
-}
-
-function batchesToProperties(batches, showing) {
-  return batches
-    .take(showing)
-    .filter(b => b)
-    .flatten(true)
-    .toJS()
-}
+    <button onClick={() => onFetchMore()}>Show More</button>
+    <button onClick={() => onToggleSort()}>Toggle Sort</button>
+  </div>
 
 export default PropertyList
--- client/src/Components/PropertyList/PropertyListContainer.js
+++ client/src/Components/PropertyList/PropertyListContainer.js
@@ -1,19 +1,50 @@
 import PropertyList from './PropertyList'
 import { connect } from 'react-redux'
-import { viewProperty, fetchEntities } from '../../Actions'
+import { fetchEntities } from '../../Actions'
+import React from 'react'
+
+/*
+  mapImmutableJsPropsToProps is a Higher-Order-Component which extracts props
+  from the ImmutableJS object passed in from the Redux container below.
+*/
+const mapImmutableJsPropsToProps = WrappedComponent => ({ immutable, ...uiEvents }) =>
+  <WrappedComponent {...{
+    properties: batchesToProperties(
+      immutable.get('batches'),
+      immutable.get('showing')
+    ),
+    ...uiEvents
+  }} />
+
+function batchesToProperties(batches, showing) {
+  return batches
+    .take(showing)
+    .filter(b => b)
+    .flatten(true)
+    .toJS()
+}
+
+/*
+  Redux Container is given the ImmutableJS structure and does not render its
+  child component (which is our mapImmutableJsPropsToProps HOC above) unless
+  it has changed.
+*/
+const mapStateToProps = state => ({
+  immutable: state.app.Property
+})
+
+const mapDispatchToProps = dispatch => ({
+  onFetchMore() {
+    dispatch(fetchEntities('Property', 'listProperties'))
+  },
+  onChangeSort(sortAsc) {
+    //dispatch(apiCall('listProperties', {}, { sortAsc }))
+  }
+})
 
 const PropertyListContainer = connect(
-  state => ({
-    immutable: state.app.Property
-  }),
-  dispatch => ({
-    onFetchMore() {
-      dispatch(fetchEntities('Property', 'listProperties'))
-    },
-    onChangeSort(sortAsc) {
-      //dispatch(apiCall('listProperties', {}, { sortAsc }))
-    }
-  })
-)(PropertyList)
+  mapStateToProps,
+  mapDispatchToProps
+)(mapImmutableJsPropsToProps(PropertyList))
 
 export default PropertyListContainer
